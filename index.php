16470369d41df79721d55e936cf0b5ed91f56a31
deyaa1997 <deyah.mosa@gmail.com>
2021-10-14 18:44:26 +0300

    finish

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+echo "hello"
+
+$player  = "player1, player2 player3, player4, player5, player6";
+$arrPlayer = explode(", ", $player);
+
+$string= "Hello World of PHP"
+$modifyString = str_replace("e", "", $string);
+
+$str = 'abcdef';
+$shuffled = str_shuffle($str);
+
+
+
+?>
